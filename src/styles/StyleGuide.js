b734ba00c991bb67098c33da5d19255cbd4aabed
Gigi Minova <minova.gigi@gmail.com>
2019-10-02 17:34:41 +0100

    fonts, colors and button classes added

---
+++ src/styles/StyleGuide.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Normalize.sass";
+import "./App.sass";
+
+const StyleGuide = () => {
+  return (
+    <div className="container">
+      <h1 className="title1">H1 heading Raleway</h1>
+      <p className="title1">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <h2 className="title2">H2 heading Montserrat</h2> 
+      <p className="title2">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <h3 className="title3">H3 heading Lato</h3>
+      <p className="title3">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <h4 className="title4">H4 heading Quicksand</h4>
+      <p className="title4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+      <div className="dark-bg-gradient--rectangle">dark-bg-gradient--rectangle</div>
+      <button className="button button--search">Search</button>
+      <button className="button button--accept">Accept</button>
+    </div>
+  );
+};
+
+export default StyleGuide;
