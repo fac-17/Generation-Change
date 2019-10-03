31da890a8babdc39805d957cbf58bee1d30db4cd
Christine <christine.altmann@gmx.com>
2019-10-03 17:52:05 +0100

    add getData airtable lambda function

--- functions/getData.js
+++ functions/getData.js
@@ -21,6 +21,22 @@ exports.handler = (event, context, callback) => {
           allRecords.push(record);
         });
         fetchNextPage();
+      }),
+      (done = err => {
+        if (err) {
+          callback(err);
+        } else {
+          const body = JSON.stringify({ records: allRecords });
+          const response = {
+            statusCode: 200,
+            body: body,
+            headers: {
+              "content-type": "application/json",
+              "cache-control": "Cache-Control: max-age=300, public"
+            }
+          };
+          callback(null, response);
+        }
       })
     );
 };
