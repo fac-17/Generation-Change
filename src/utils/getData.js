67fe46df400a55f277fd035b18911466f66bd15a
Sam Harris <sam@jam-bc.com>
2019-10-08 15:17:43 +0100

    fixed fetch call to use full url as redirects were not working

--- src/utils/getData.js
+++ src/utils/getData.js
@@ -1,7 +1,7 @@
 // need to import some component that calls the fetch;
 
 const getData = setData => {
-  return fetch(`/getdata`)
+  return fetch(`/.netlify/functions/getdata`)
     .then(result => result.json())
     .then(result => {
       setData(result.records);
