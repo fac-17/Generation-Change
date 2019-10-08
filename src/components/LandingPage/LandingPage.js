e4491d012a5600122a6ff4fdba8a7d4bacf97271
Christine <christine.altmann@gmx.com>
2019-10-08 17:55:43 +0100

    merge master

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Navbar from "./Navbar";
-import Searchbar from "./Searchbar";
+import Searchbar from "../Universal/Searchbar";
 import Stories from "./Stories";
 
 const LandingPage = () => {
