7678bbdd44eb1ee55508efacd852fc8813a04fee
Christine <christine.altmann@gmx.com>
2019-10-08 17:17:06 +0100

    add routes to details page Relates #31

--- src/components/DetailsPage/DetailsPage.js
+++ src/components/DetailsPage/DetailsPage.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 const DetailsPage = () => {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 };
 
 export default DetailsPage;
