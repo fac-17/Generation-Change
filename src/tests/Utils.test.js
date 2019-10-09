a859443b2c31b6234e970b93a54292029f9d59ad
Sam Harris <sam@jam-bc.com>
2019-10-09 15:24:35 +0100

    ensure convertPostcode is pure function

--- src/tests/Utils.test.js
+++ src/tests/Utils.test.js
@@ -3,7 +3,9 @@ import ConvertPostcode from "../Utils/ConvertPostcode";
 
 test("ConvertPostcode returns correct longitude and latitude ", () => {
   const expected = { longitude: -2.008298, latitude: 50.712412 };
-  return ConvertPostcode("BH15 4DH").then(actual => {
+  const initialPostcode = "BH15 4DH";
+  return ConvertPostcode(initialPostcode).then(actual => {
     expect(actual).toEqual(expected);
+    expect("BH15 4DH").toEqual(initialPostcode);
   });
 });
