987ef379999f987ad8f24c3d0dd81cda04aaec2f
georgia.shaw <georgia@cranbrookw4.com>
2019-10-10 11:23:03 +0100

    google maps changes

---
+++ src/utils/getGoogleMaps.js
@@ -0,0 +1,12 @@
+const getGoogleMaps = () => {
+  return fetch("./.netlify/functions/googleMaps/googleMaps")
+    .then(response => response.json())
+    .then(({ result: { latitude, longitude } }) => {
+      return { longitude, latitude };
+    })
+    .catch(err => {
+      return err;
+    });
+};
+
+module.exports = convertPostcode;
