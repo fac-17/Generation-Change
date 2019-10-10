f8a251c4ecb9ee1d2e35617792baaac0eb342100
Sam Harris <sam@jam-bc.com>
2019-10-10 09:33:41 +0200

    test that search bar calls the results page when given a correct postcode

--- src/tests/UniversalComponents.test.js
+++ src/tests/UniversalComponents.test.js
@@ -10,27 +10,15 @@ import Navbar from "../components/Universal/Navbar";
 import { createMemoryHistory } from "history";
 import { Router, MemoryRouter } from "react-router-dom";
 // import ReactDOM from "react-dom";
-// import { convertPostcode } from "../utils/convertPostcode";
 
 afterEach(cleanup);
 
-// const mockConvertPostcode = jest.fn();
-
-// jest.mock("../utils/convertPostcode", () => {
-//   return jest.fn().mockImplementation(() => {
-//     return { convertPostcode: mockConvertPostcode };
-//   });
-// });
-
-// beforeEach(() => {
-//   mockConvertPostcode.mockClear();
-// });
-
 describe.only("SearchBar behaves as expected", () => {
-  const { getByLabelText, getByTestId } = render(
-    <MemoryRouter>
+  const history = createMemoryHistory();
+  const { getByLabelText, getByTestId, getByDisplayValue } = render(
+    <Router history={history}>
       <SearchBar />
-    </MemoryRouter>
+    </Router>
   );
   const input = getByLabelText("Postcode Search");
   const submitButton = getByTestId("searchbar--search-button");
@@ -41,15 +29,16 @@ describe.only("SearchBar behaves as expected", () => {
       target: { value: fourLetterPostcode }
     });
     fireEvent.click(submitButton);
-    // expect(getByDisplayValue(fourLetterPostcode)).toBeTruthy();
+    expect(getByDisplayValue(fourLetterPostcode)).toBeTruthy();
   });
 
   test("SearchBar will search for a postcode 5 char and above", () => {
     const fullPostcode = "EC25HY";
     fireEvent.change(input, { target: { value: fullPostcode } });
     fireEvent.click(submitButton);
-    // expect(mockConvertPostcode).toHaveBeenCalledTimes(1);
-    // expect(getByDisplayValue(fullPostcode).exists()).toBeFalsy();
+    setTimeout(() => {
+      expect(history.location.pathname).toBe("/results");
+    }, 1000);
   });
 });
 
