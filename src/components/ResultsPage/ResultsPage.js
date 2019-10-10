145f9d6bd0d83a78dedd6793a7c6d75a2b70c19c
Christine <christine.altmann@gmx.com>
2019-10-10 17:12:04 +0100

    create longitude and latitude object from airtable data

--- src/components/ResultsPage/ResultsPage.js
+++ src/components/ResultsPage/ResultsPage.js
@@ -4,15 +4,44 @@ import { Link } from "react-router-dom";
 import { getDistance } from "geolib";
 
 const ResultsPage = ({ geocode, data }) => {
-  if (data) {
-    console.log({ geocode });
-    const receivedData = data;
-    console.log("receiveddata", receivedData[0]);
-  }
+  const longs = data.reduce((acc, curr) => {
+    return curr.fields.longitude != null
+      ? acc.concat(curr.fields.longitude)
+      : acc;
+  }, []);
+  // console.log("sortelistingslongs", longs);
 
-  // const sortedListings = receivedData.map(listing => {
-  //   console.log(listing.fields.latitude);
+  const coords = data.reduce((acc, curr) => {
+    return curr.fields.latitude !== undefined ||
+      curr.fields.longitude !== undefined
+      ? acc.concat(
+          Object.fromEntries(
+            new Map([
+              ["latitude", curr.fields.latitude],
+              ["longitude", curr.fields.longitude]
+            ])
+          )
+        )
+      : acc;
+  }, []);
+  console.log("coords", coords);
+
+  // const array = [];
+  // data.map((e, index) => {
+  //   console.log(e.fields.latitude[index]);
+  //   return e.fields.latitude[index] != null
+  //     ? console.log("not in")
+  //     : // e.fields.longitude[index] != null
+  //       //   ? console.log(e, index)
+  //       //   : console.log("not in")
+  //       console.log("not in 2");
+
+  //   // Object.fromEntries(
+  //   //   new Map([e.fields.latitude[index]], [e.fields.longitude[index]])
+  //   // );
   // });
+
+  // console.log("fromentries", array);
 
   console.log(
     "distance",
