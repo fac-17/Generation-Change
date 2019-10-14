e43bf7d04cfbd292e47f8cb6057bb9d9065bbccb
Gigi Minova <minova.gigi@gmail.com>
2019-10-14 14:24:52 +0100

    render project details

--- src/components/DetailsPage/ProjectDetails.js
+++ src/components/DetailsPage/ProjectDetails.js
@@ -2,40 +2,40 @@ import React from "react";
 import Svg from "../Universal/Svg";
 
 const ProjectDetails = ({detailsData}) => {
-  if (!data) {
+  if (!detailsData) {
     return <h1>...loading</h1>;
   }
   return (
     <div className="project-short-summary-grid">
       <div className="project-title-cat">
-        <div>category</div>
-        <div>title</div>
+        <div>{detailsData.fields.category}</div>
+        <div>{detailsData.fields.title}</div>
       </div>
       <div className="project-bulletpoints">
-        <div>{detailsData.fields.title}</div>
+        <div>tagline</div>
         <div className="project-card__icon-info-outer-wrap">
           <div className="project-card__icon-info-individual-wrap">
             <Svg color="--white" height="20" width="20" icon="age" />
             <p>Age</p>
-            <div>minimum_age-maximum_age</div>
+            <div>{detailsData.fields.minimum_age}-{detailsData.fields.maximum_age}</div>
           </div>
 
           <div className="project-card__icon-info-individual-wrap">
             <Svg color="--white" height="20" width="20" icon="group-size" />
             <p>Group Size</p>
-            <div>group_size</div>
+            <div>{detailsData.fields.group_size}</div>
           </div>
 
           <div className="project-card__icon-info-individual-wrap">
             <Svg color="--white" height="20" width="20" icon="duration" />
             <p>Duration</p>
-            <div>session_duration_hours hours</div>
+            <div>{detailsData.fields.session_duration_hours} hours</div>
           </div>
 
           <div className="project-card__icon-info-individual-wrap">
             <Svg color="--white" height="20" width="20" icon="calendar" />
             <p>Repeats</p>
-            <div>session_frequency_per_week a week</div>
+            <div>{detailsData.fields.session_frequency_per_week} a week</div>
           </div>
         </div>
       </div>
