ecd5d1c1d6eb535ae01597d04e6cd105112cc663
georgia.shaw <georgia@cranbrookw4.com>
2019-10-15 16:34:08 +0100

    add marker to map

--- src/components/ResultsPage/LeafletMap.js
+++ src/components/ResultsPage/LeafletMap.js
@@ -2,10 +2,12 @@ import React, { useEffect } from "react";
 import L from "leaflet";
 import { Map, Marker, Popup, TileLayer, Tooltip } from "react-leaflet";
 
-function LeafletMap() {
+const LeafletMap = () => {
+  const markerPosition = [51.5007, -0.1246];
+  // create map
+  const mapRef = React.useRef(null);
   useEffect(() => {
-    // create map
-    L.map("map", {
+    mapRef.current = L.map("map", {
       center: [51.5007, -0.1246],
       zoom: 16,
       layers: [
@@ -17,7 +19,20 @@ function LeafletMap() {
     });
   }, []);
 
+  // add marker
+  const markerRef = React.useRef(null);
+  useEffect(
+    () => {
+      if (markerRef.current) {
+        markerRef.current.setLatLng(markerPosition);
+      } else {
+        markerRef.current = L.marker(markerPosition).addTo(mapRef.current);
+      }
+    },
+    [markerPosition]
+  );
+
   return <div id="map" />;
-}
+};
 
 export default LeafletMap;
