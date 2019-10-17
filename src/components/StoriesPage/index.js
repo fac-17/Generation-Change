d8f3ff6d8a1f78bb58748181f8bd2c65f57667e2
Gigi Minova <minova.gigi@gmail.com>
2019-10-17 15:49:03 +0100

    style sotry page

--- src/components/StoriesPage/index.js
+++ src/components/StoriesPage/index.js
@@ -3,25 +3,41 @@ import Navbar from "../Universal/Navbar";
 import { Link } from "react-router-dom";
 
 const StoriesPage = ({ data }) => {
-  console.log(data[85].fields);
+  console.log(data[85].fields.charity_about);
   return (
     <>
       <Navbar />
       <div className="wrapper__story">
-        <Link to={{ pathname: "/details" }}>
-          <h1>{data[85].fields.story_title}</h1>
-        </Link>
         <img
           src={data[85].fields.story_photo_1}
           alt="story"
           className="story__img--page"
         />
+        <Link to={{ pathname: "/details" }}
+        className="story__link">
+          <h1 className="title1">{data[85].fields.story_title}</h1>
+        </Link>
         <p className="category--story">{data[85].fields.category}</p>
         <div className="wrapper">
           <h2>What happened: </h2>
           <p>{data[85].fields.story_what}</p>
           <h2>The Impact: </h2>
-          <p>{data[85].fields.story_results}</p>
+          <div className="story-impact">
+            <p className="story-impact-txt">
+              <b className="story-impact-num">80</b>
+              <p>hours contributed</p>
+            </p>
+            <p className="story-impact-txt">
+              <b className="story-impact-num">40</b>
+              <p>nights away from home</p>
+            </p>
+            <p className="story-impact-txt">
+              <b className="story-impact-num">£2,000</b>
+              <p>saved</p>
+            </p>
+          </div>
+          <h2>The Charity: </h2>
+          <p>{data[85].fields.charity_about}</p>
         </div>
       </div>
     </>
