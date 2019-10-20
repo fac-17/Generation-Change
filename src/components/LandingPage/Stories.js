3ebdded7635c03225ad0e8ca139df52aacb6bca8
Christine <christine.altmann@gmx.com>
2019-10-20 15:03:44 +0100

    small style change

--- src/components/LandingPage/Stories.js
+++ src/components/LandingPage/Stories.js
@@ -5,7 +5,7 @@ const Stories = ({ data }) => {
   console.log(data[85].fields.story_photo_1);
   return (
     <>
-      <p className="word">Featured stories</p>
+      <p className="featured-stories">FEARTURED STORIES</p>
       <hr className="line" />
       <div className="container">
         <Link to={{ pathname: "/stories" }} className="container__story--0">
@@ -15,29 +15,23 @@ const Stories = ({ data }) => {
               alt="story"
               className="story__img"
             />
-            <h1 className="story__title--landing">{data[85].fields.story_title}</h1>
+            <h1 className="story__title--landing">
+              {data[85].fields.story_title}
+            </h1>
             <p className="category--landing">{data[85].fields.category}</p>
           </article>
         </Link>
 
-          <article className="container__story--1">
-            <img
-              src="statics/story2.jpg"
-              alt="story"
-              className="story__img"
-            />
-            <h1>Princes Trust bring hospital garden back to life</h1>
-            <p className="category--landing">Volunteering</p>
-          </article>
-          <article className="container__story--2">
-            <img
-              src='statics/story3.jpg'
-              alt="story"
-              className="story__img"
-            />
-            <h1>Change society and the environment</h1>
-            <p className="category--landing">Fundraising</p>
-          </article>
+        <article className="container__story--1">
+          <img src="statics/story2.jpg" alt="story" className="story__img" />
+          <h1>Princes Trust bring hospital garden back to life</h1>
+          <p className="category--landing">Volunteering</p>
+        </article>
+        <article className="container__story--2">
+          <img src="statics/story3.jpg" alt="story" className="story__img" />
+          <h1>Change society and the environment</h1>
+          <p className="category--landing">Fundraising</p>
+        </article>
       </div>
     </>
   );
