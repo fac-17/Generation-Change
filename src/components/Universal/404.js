e2d8c801f7fd8b4719ee061ce183c9e473c77dd7
Gigi Minova <minova.gigi@gmail.com>
2019-10-20 19:00:57 +0100

    add back to home button on 404 route

--- src/components/Universal/404.js
+++ src/components/Universal/404.js
@@ -1,7 +1,14 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 const notFound = () => {
-  return <h1 className="notfound">404 Page not found</h1>;
+  return (
+    <div>
+      <Link to={{ pathname: "/" }}>
+        <h1 className="notfound">404 Page not found</h1>
+        <button className="button button--home">Back to Home</button>
+      </Link>
+    </div>
+  );
 };
 
 export default notFound;
